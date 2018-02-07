30da290a66c2ea974f629b9b0f1490b11ab293d0
dalia ramos and Kayla Ondracek <dalia@>
2018-02-07 10:40:36 -0800

    add form to collect job info, display job info confirmation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
-using Systme.Collections.Generic;
-//using Jobs.Models;
+using System.Collections.Generic;
+using Jobs.Models;
 
 namespace Jobs.Controllers
 {
   public class HomeController : Controller
   {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
 
+    [HttpGet("/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/addjob")]
+    public ActionResult CreateJob()
+    {
+      Job newJob = new Job(
+        Request.Form["job-title"],
+        Request.Form["company-name"],
+        Request.Form["location"],
+        Request.Form["start-date"],
+        Request.Form["end-date"],
+        Request.Form["desc"]
+      );
+      return View("Confirmation", newJob);
+    }
   }
 }
--- Models/Jobs.cs
+++ Models/Jobs.cs
@@ -2,6 +2,82 @@ namespace Jobs.Models
 {
   public class Job
   {
-    
+    private string _jobTitle;
+    private string _companyName;
+    private string _location;
+    private string _startDate;
+    private string _endDate;
+    private string _description;
+
+    public Job(string jobTitle, string companyName, string location, string start, string end, string desc)
+    {
+      _jobTitle = jobTitle;
+      _companyName = companyName;
+      _location = location;
+      _startDate = start;
+      _endDate = end;
+      _description = desc;
+    }
+
+    public string GetJobTitle()
+    {
+      return _jobTitle;
+    }
+
+    public void SetJobTitle(string jobTitle)
+    {
+      _jobTitle = jobTitle;
+    }
+
+    public string GetCompanyName()
+    {
+      return _companyName;
+    }
+
+    public void SetCompanyName(string companyName)
+    {
+      _companyName = companyName;
+    }
+
+    public string GetLocation()
+    {
+      return _location;
+    }
+
+    public void SetLocation(string location)
+    {
+      _location = location;
+    }
+
+    public string GetStartDate()
+    {
+      return _startDate;
+    }
+
+    public void SetStartDate(string startDate)
+    {
+      _startDate = startDate;
+    }
+
+    public string GetEndDate()
+    {
+      return _endDate;
+    }
+
+    public void SetEndDate(string endDate)
+    {
+      _endDate = endDate;
+    }
+
+    public string GetDescription()
+    {
+      return _description;
+    }
+
+    public void SetDescription(string desc)
+    {
+      _description = desc;
+    }
   }
+
 }
